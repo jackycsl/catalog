af1bb63e9b09230f29fb1df743edf0adb41130ce
Jacky CSL <jackycsl@Jackys-MacBook-Pro-2.local>
2022-02-12 16:53:35 +0800

    add log middleware to catalog service

--- catalog-service/internal/server/grpc.go
+++ catalog-service/internal/server/grpc.go
@@ -3,6 +3,7 @@ package server
 import (
 	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
@@ -24,6 +25,7 @@ func NewGRPCServer(c *conf.Server, catalog *service.CatalogService, tp *tracesdk
 			),
 			tracing.Server(
 				tracing.WithTracerProvider(tp)),
+			logging.Server(logger),
 		),
 	}
 	if c.Grpc.Network != "" {
--- catalog-service/internal/server/http.go
+++ catalog-service/internal/server/http.go
@@ -3,6 +3,7 @@ package server
 import (
 	prom "github.com/go-kratos/kratos/contrib/metrics/prometheus/v2"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/metrics"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
 	"github.com/go-kratos/kratos/v2/middleware/tracing"
@@ -26,6 +27,7 @@ func NewHTTPServer(c *conf.Server, catalog *service.CatalogService, tp *tracesdk
 			),
 			tracing.Server(
 				tracing.WithTracerProvider(tp)),
+			logging.Server(logger),
 		),
 	}
 	if c.Http.Network != "" {
